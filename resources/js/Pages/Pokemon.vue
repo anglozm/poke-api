c452bcb1de97cfadf14bec210bd6189dfcfb8059
Angel Lozada <alozada@simplify.co.tz>
2024-02-20 17:01:12 -0400

    KEETUP-0002: Added preview for a pokemon when is selected

--- resources/js/Pages/Pokemon.vue
+++ resources/js/Pages/Pokemon.vue
@@ -15,14 +15,16 @@ import DangerButton from "@/Components/DangerButton.vue";
 const pokeId = ref(0);
 const pokemon = ref([]);
 const pokemonType = ref([]);
+const pokeSelected = ref({});
 
 const emptyPokeList = ref(false);
 
 const nameInput = ref([]);
 
-const confirmingPokemonCreate = ref(false);
-const confirmingPokemonUpdate = ref(false);
-const confirmingPokemonDelete = ref(false);
+const showingPokemonDetails = ref(false);
+const isPokemonCreate = ref(false);
+const isPokemonUpdate = ref(false);
+const isPokemonDelete = ref(false);
 
 const form = useForm({
     name: '',
@@ -34,11 +36,11 @@ const getPokemonList = async () => {
     try {
         const res = await axios.get('/api/pokemon');
         const results = sortByName(res.data.data);
-        console.log('Pokemon', results);
+        console.log('Pokemon:', results);
 
         return results;
     } catch (error) {
-        console.error('Error getting pokemon', error);
+        console.error('Error getting pokemon list', error);
 
         throw error;
     }
@@ -48,11 +50,11 @@ const fetchPokemonTypeList = async () => {
     try {
         const res = await axios.get('https://pokeapi.co/api/v2/type');
         const results = sortByName(res.data.results);
-        console.log('Pokemon types', results);
+        console.log('Pokemon types:', results);
 
         return results;
     } catch (error) {
-        console.error('Error getting pokemon', error);
+        console.error('Error fetching pokemon type list', error);
 
         throw error;
     }
@@ -68,11 +70,10 @@ const fetchPokeImageByName = async () => {
         const pokeImage = poke.data.sprites.other.dream_world.front_default;
         console.log('Fetch "' + pokeName + '" by name:', pokeImage);
 
-        return pokeImage
+        return pokeImage;
     } catch (error) {
-        console.error('Error fetching pokemon. Pokemon not found by name', error);
-
-        throw error;
+        console.error('Pokemon not found by name', error);
+        return '/images/pokeball.png';
     }
 }
 
@@ -84,25 +85,31 @@ onMounted(async () => {
 
 //========================================================================
 const sortByName = (arr) => arr.sort((a, b) => (a.name > b.name) ? 1 : -1);
-
 //========================================================================
-const confirmPokemonCreate = () => {
-    confirmingPokemonCreate.value = true;
+const showModalPokemonCreate = () => {
+    isPokemonCreate.value = true;
     nextTick(() => nameInput.value.focus());
     console.log('On create confirm...');
 };
 
-const confirmPokemonUpdate = (poke) => {
-    confirmingPokemonUpdate.value = true;
+const showModalPokemonUpdate = (poke) => {
+    isPokemonUpdate.value = true;
     nextTick(() => nameInput.value.focus());
     pokeId.value = poke.id;
     console.log('On update confirm... Id:', pokeId.value);
 };
 
-const confirmPokemonDelete = (poke) => {
-    confirmingPokemonDelete.value = true;
+const showModalPokemonDelete = (poke) => {
+    isPokemonDelete.value = true;
     pokeId.value = poke.id;
     console.log('On delete confirm... Id:', pokeId.value);
+};
+
+const showModalPokemonDetails = async (poke) => {
+    showingPokemonDetails.value = true;
+    pokeId.value = poke.id;
+    pokeSelected.value = pokemon.value.find((item) => item.id === poke.id);
+    console.log('On showing pokemon details... Id:', pokeId.value);
 };
 //========================================================================
 const submitPokemonCreate = async () => {
@@ -131,7 +138,6 @@ const submitPokemonUpdate = async () => {
         });
 
         pokemon.value = await getPokemonList();
-        emptyPokeList.value = pokemon.value.length !== 0;
 
         console.log('... Update confirmed. Id:', pokeId.value);
     } catch (error) {
@@ -156,18 +162,22 @@ const submitPokemonDelete = async () => {
 };
 //========================================================================
 const closeModalCreate = async () => {
-    confirmingPokemonCreate.value = false;
+    isPokemonCreate.value = false;
     form.reset();
 };
 
 const closeModalUpdate = () => {
-    confirmingPokemonUpdate.value = false;
+    isPokemonUpdate.value = false;
     form.reset();
 };
 
 const closeModalDelete = () => {
-    confirmingPokemonDelete.value = false;
+    isPokemonDelete.value = false;
     form.reset();
+};
+
+const closeModalDetails = async () => {
+    showingPokemonDetails.value = false;
 };
 </script>
 
@@ -179,7 +189,7 @@ const closeModalDelete = () => {
             <section class="flex justify-between px-4 w-[800px] rounded-md shadow-md hover:shadow-lg bg-gray-50">
                 <h2 class="flex items-center py-4 font-semibold text-xl text-gray-800 leading-tight">Poke List</h2>
                 <div class="flex py-4">
-                    <PrimaryButton class="ml-4" @click="confirmPokemonCreate()">
+                    <PrimaryButton class="ml-4" @click="showModalPokemonCreate()">
                         Add Pokemon
                     </PrimaryButton>
                 </div>
@@ -190,17 +200,25 @@ const closeModalDelete = () => {
             <section v-if="emptyPokeList" class="flex justify-between px-4 m-2 pb-2 w-[800px] rounded-md shadow-md hover:shadow-lg bg-gray-200">
                 <ul class="py-2">
                     <li v-for="(poke, index) in pokemon" :key="index" class="motion-safe:hover:scale-[1.01] rounded-md mt-2 bg-gray-50">
-                        <section class="flex justify-between w-[770px] rounded-md shadow-md hover:shadow-2xl">
-                            <img :src="poke.photo" alt="Pokemon logo" class="flex-none w-20 h-20 m-6 bg-gray-50"/>
-                            <div class="flex-auto self-center m-1">
-                                <ul class="py-4">
-                                    <li>
-                                        <b>{{ poke.name }}</b>
-                                    </li>
-                                    <li>
-                                        <i>{{ poke.type }}</i>
-                                    </li>
-                                </ul>
+                        <section class="flex justify-between rounded-md shadow-md hover:shadow-2xl">
+                            <div class="w-[740px]">
+                                <a
+                                    @click="showModalPokemonDetails(poke)"
+                                    target="_blank"
+                                    class="flex rounded-lg"
+                                >
+                                    <img :src="poke.photo" alt="Pokemon logo" class="flex-none w-20 h-20 m-6 bg-gray-50"/>
+                                    <div class="flex-auto self-center m-1">
+                                        <ul class="py-4">
+                                            <li>
+                                                <b>{{ poke.name }}</b>
+                                            </li>
+                                            <li>
+                                                <i>{{ poke.type }}</i>
+                                            </li>
+                                        </ul>
+                                    </div>
+                                </a>
                             </div>
                             <div class="pt-3">
                                 <Dropdown align="right" width="48">
@@ -216,13 +234,13 @@ const closeModalDelete = () => {
 
                                     <template #content>
                                         <button
-                                            @click="confirmPokemonUpdate(poke)"
+                                            @click="showModalPokemonUpdate(poke)"
                                             class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                                         >
                                             Edit
                                         </button>
                                         <button
-                                            @click="confirmPokemonDelete(poke)"
+                                            @click="showModalPokemonDelete(poke)"
                                             class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 transition duration-150 ease-in-out"
                                         >
                                             Delete
@@ -239,7 +257,7 @@ const closeModalDelete = () => {
             </label>
         </div>
 
-        <Modal :show="confirmingPokemonCreate" @close="closeModalCreate">
+        <Modal :show="isPokemonCreate" @close="closeModalCreate()">
             <form @submit.prevent="submitPokemonCreate()" class="m-6">
                 <div class="mt-10">
                     <InputLabel for="name" value="Name" />
@@ -279,7 +297,7 @@ const closeModalDelete = () => {
                 </div>
 
                 <div class="flex items-center justify-end pt-8 pb-4">
-                    <SecondaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="closeModalCreate">
+                    <SecondaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="closeModalCreate()">
                         Cancel
                     </SecondaryButton>
 
@@ -290,7 +308,7 @@ const closeModalDelete = () => {
             </form>
         </Modal>
 
-        <Modal :show="confirmingPokemonUpdate" @close="closeModalUpdate">
+        <Modal :show="isPokemonUpdate" @close="closeModalUpdate()">
             <form @submit.prevent="submitPokemonUpdate()" class="m-6">
                 <div class="mt-10">
                     <InputLabel for="name" value="Name" />
@@ -330,7 +348,7 @@ const closeModalDelete = () => {
                 </div>
 
                 <div class="flex items-center justify-end pt-8 pb-4">
-                    <SecondaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="closeModalUpdate">
+                    <SecondaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="closeModalUpdate()">
                         Cancel
                     </SecondaryButton>
 
@@ -341,12 +359,12 @@ const closeModalDelete = () => {
             </form>
         </Modal>
 
-        <Modal :show="confirmingPokemonDelete" @close="closeModalDelete">
+        <Modal :show="isPokemonDelete" @close="closeModalDelete()">
             <h3 class="flex items-center p-10 font-semibold text-xl text-gray-800 leading-tight">
                 Are you sure you want to delete this pokemon?
             </h3>
             <div class="flex items-center justify-end p-4">
-                <SecondaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="closeModalDelete">
+                <SecondaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing" @click="closeModalDelete()">
                     Cancel
                 </SecondaryButton>
 
@@ -354,6 +372,26 @@ const closeModalDelete = () => {
                     Delete
                 </DangerButton>
             </div>
+        </Modal>
+
+        <Modal :show="showingPokemonDetails" @close="closeModalDetails()">
+            <section class="p-1.5 bg-blue-700">
+                <div class="flex justify-start bg-amber-300">
+                    <div class="flex items-center shrink-0 p-5">
+                        <img src="/images/pokeball.png" alt="Pokemon logo" class="w-10 h-10" />
+                    </div>
+                    <h3 class="flex items-center  font-semibold text-3xl text-gray-800 leading-tight">
+                        {{ pokeSelected.name }}: {{ pokeSelected.type }}
+                    </h3>
+                </div>
+                <div class="p-1">
+                </div>
+                <div class="flex justify-center bg-white">
+                    <div class="flex self-center p-10">
+                        <img :src="pokeSelected.photo" alt="Pokemon logo" class="flex self-center w-[500px]"/>
+                    </div>
+                </div>
+            </section>
         </Modal>
     </AuthenticatedLayout>
 </template>
